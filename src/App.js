caa01727c1dace8ebcf547b32d1d54603f4c7d01
yuiceline <yuiserikawa@gmail.com>
2023-02-01 14:51:13 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://preeminent-macaron-0a3447.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Yui Serikawa{" "}
-        </a>{" "}
-        and is open-sourced on {""}
-        <a
-          href="https://github.com/yuiscodes/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://preeminent-macaron-0a3447.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Yui Serikawa{" "}
+          </a>{" "}
+          and is open-sourced on {""}
+          <a
+            href="https://github.com/yuiscodes/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
