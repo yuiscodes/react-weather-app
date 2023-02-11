5da6c2db24a00eaeed5e5b578a43602886be3889
yuiceline <yuiserikawa@gmail.com>
2023-02-11 23:47:04 +0900

    Added external icons

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -11,9 +12,10 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img src={props.data.iconUrl} alt="Clear icon" />
+      <div className="row mt-3 ">
+        <div className="col-6 align-self-end">
+          <WeatherIcon code={props.data.icon} />
+
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>
