3d2cac393be4f350a2204d80dcfee7d4d1c11edb
yuiceline <yuiserikawa@gmail.com>
2023-03-04 12:16:50 +0900

    Used loop to add 5 days forecasts

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -9,18 +9,23 @@ export default function WeatherForecast(props) {
   let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-   setForecast(response.data.daily);
-   setLoaded(true);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
   if (loaded) {
-
     return (
-      <div className="WeatherForecast">
-        <div clcassName="row">
-          <div className="col">
-        <WeatherForecastDay data={forecast[0]}/>
-          </div>
+      <div className="WeatherForecast mt-3">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
