250dafb6bab7d9423cdcbf69043d981326c5e97d
yuiceline <yuiserikawa@gmail.com>
2023-04-06 16:53:51 +0900

    Refactored code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         
-        <Weather defaultCity="Osaka"/>
+        <Weather defaultCity="Kyoto"/>
         <footer>
           This project was coded by{" "}
           <a
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,15 @@ export default function WeatherForecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+   
+  function load() {
+      let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+      let longitude = props.coordinates.lon;
+      let latitude = props.coordinates.lat;
+      let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+      axios.get(apiUrl).then(handleResponse);
+  }
 
   if (loaded) {
     return (
@@ -28,19 +37,16 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
+  load();
+  
     return null;
   }
 }
